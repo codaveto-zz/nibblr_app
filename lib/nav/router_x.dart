ddf6a1fadf8b1624f2e77303e728ec96428aac5e
Brian Manuputty <info@deappspecialist.nl>
2020-12-23 18:09:23 +0100

    Forgotten files

--- lib/nav/router_x.dart
+++ lib/nav/router_x.dart
@@ -1,40 +1,40 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:auto_route/auto_route_annotations.dart';
-import 'package:nibblr_app/nav/transitions.dart';
-import 'package:nibblr_app/ui/views/home/home_view.dart';
-import 'package:nibblr_app/ui/views/login/login_view.dart';
-import 'package:nibblr_app/ui/views/signup/signup_view.dart';
-import 'package:nibblr_app/ui/views/startup/startup_view.dart';
-import 'package:nibblr_app/util/constants/sizes.dart';
-
-@CustomAutoRouter(
-  routes: <AutoRoute>[
-    CustomRoute(
-        page: LoginView,
-        name: 'loginView',
-        transitionsBuilder: TransitionsBuilders.fadeIn,
-        durationInMilliseconds: CustomSize.defaultAnimationDuration),
-    CustomRoute(
-      page: HomeView,
-      name: 'homeView',
-      transitionsBuilder: TransitionMaster.sharedAxisTransition,
-      durationInMilliseconds: CustomSize.defaultAnimationDuration,
-    ),
-    CustomRoute(
-      page: StartupView,
-      name: 'startupView',
-      transitionsBuilder: TransitionMaster.sharedAxisTransition,
-      durationInMilliseconds: CustomSize.defaultAnimationDuration,
-    ),
-    CustomRoute(
-      page: SignupView,
-      name: 'signupView',
-      transitionsBuilder: TransitionsBuilders.fadeIn,
-      durationInMilliseconds: CustomSize.defaultAnimationDuration,
-    ),
-  ],
-)
-class $Router {}
-
-// ADD COMMENTS AND REIMPORT TRANSITION MASTER @ ROUTER.GR.DART TO AVOID FAULTY IMPORTS
-// ROUTER.GR.DART IS ADDED TO VERSION CONTROL
+// import 'package:auto_route/auto_route.dart';
+// import 'package:auto_route/auto_route_annotations.dart';
+// import 'package:nibblr_app/nav/transitions.dart';
+// import 'package:nibblr_app/ui/views/home/home_view.dart';
+// import 'package:nibblr_app/ui/views/login/login_view.dart';
+// import 'package:nibblr_app/ui/views/signup/signup_view.dart';
+// import 'package:nibblr_app/ui/views/startup/startup_view.dart';
+// import 'package:nibblr_app/util/constants/sizes.dart';
+//
+// @CustomAutoRouter(
+//   routes: <AutoRoute>[
+//     CustomRoute(
+//         page: LoginView,
+//         name: 'loginView',
+//         transitionsBuilder: TransitionsBuilders.fadeIn,
+//         durationInMilliseconds: CustomSize.defaultAnimationDuration),
+//     CustomRoute(
+//       page: HomeView,
+//       name: 'homeView',
+//       transitionsBuilder: TransitionMaster.sharedAxisTransition,
+//       durationInMilliseconds: CustomSize.defaultAnimationDuration,
+//     ),
+//     CustomRoute(
+//       page: StartupView,
+//       name: 'startupView',
+//       transitionsBuilder: TransitionMaster.sharedAxisTransition,
+//       durationInMilliseconds: CustomSize.defaultAnimationDuration,
+//     ),
+//     CustomRoute(
+//       page: SignupView,
+//       name: 'signupView',
+//       transitionsBuilder: TransitionsBuilders.fadeIn,
+//       durationInMilliseconds: CustomSize.defaultAnimationDuration,
+//     ),
+//   ],
+// )
+// class $Router {}
+//
+// // ADD COMMENTS AND REIMPORT TRANSITION MASTER @ ROUTER.GR.DART TO AVOID FAULTY IMPORTS
+// // ROUTER.GR.DART IS ADDED TO VERSION CONTROL
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -4,7 +4,6 @@ import 'package:nibblr_app/data/model/dinner.dart';
 import 'package:nibblr_app/nav/router.dart';
 import 'package:nibblr_app/services/dinner/dinner_service.dart';
 import 'package:nibblr_app/services/log/logger_service.dart';
-import 'package:nibblr_app/services/login/user_service.dart';
 import 'package:nibblr_app/services/token/token_service.dart';
 import 'package:nibblr_app/util/injection/locator.dart';
 import 'package:stacked/stacked.dart';
@@ -18,10 +17,8 @@ class HomeViewModel extends BaseViewModel {
   // --------------- INIT --------------- INIT --------------- INIT --------------- \\
 
   void initialise() async {
+    await _initDinners();
     _log.i('I am initialized');
-    _dinners = await runBusyFuture(_dinnerService.getUpcomingDinners());
-    Get.snackbar('Welcome', 'Login success!');
-    notifyListeners();
   }
 
   @override
@@ -36,21 +33,21 @@ class HomeViewModel extends BaseViewModel {
 
   void dinnerTapped() {}
 
-  Future<void> initDinners() async {}
+  Future<void> _initDinners() async {
+    _dinners = await runBusyFuture(_dinnerService.getUpcomingDinners());
+  }
 
   // --------------- GET & SET --------------- GET & SET --------------- GET & SET --------------- \\
 
   List<Dinner> get dinners => _dinners;
 
-  void refresh() {
+  Future<void> refresh() async {
+    await _initDinners();
   }
 
   Future<void> logout() async {
     final result = await showOkCancelAlertDialog(
-        context: Get.overlayContext,
-        title: 'Logout',
-        message:
-        'Are you sure you want to logout?');
+        context: Get.overlayContext, title: 'Logout', message: 'Are you sure you want to logout?');
     if (result == OkCancelResult.ok) {
       TokenService().deleteToken();
       Get.offAndToNamed(Routes.loginView);
